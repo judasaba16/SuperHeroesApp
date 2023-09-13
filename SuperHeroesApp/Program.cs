380227eb14201a12e8551b4c4ec73c5473fe18c2
judasaba16 <juan14san@gmail.com>
2023-09-13 12:24:20 -0500

    prueba1

--- SuperHeroesApp/Program.cs
+++ SuperHeroesApp/Program.cs
@@ -1,5 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
 
 var poderVolar = new SuperPoder();
 poderVolar.Nombre = "Volar";
@@ -10,7 +10,19 @@ var superFuerza = new SuperPoder();
 superFuerza.Nombre = "Super Fuerza";
 superFuerza.Nivel = NivelPoder.NivelTres;
 
+var inmortal = new SuperPoder();
+inmortal.Nombre = "Inmortal";
+inmortal.Descripcion = "No pueden matarlo";
+inmortal.Nivel = NivelPoder.NivelTres;
+
+var superRegeneracion = new SuperPoder();
+superRegeneracion.Nombre = "Regeneracion";
+superRegeneracion.Descripcion = "Regenera cualquier parte de sus cuerpo";
+superRegeneracion.Nivel = NivelPoder.NivelTres;
+
 var spiderman = new SuperHeroe();
+var aironman = new SuperHeroe();
+var hulk = new SuperHeroe();
 
 
 spiderman.Id = 1;
@@ -22,8 +34,32 @@ spiderman.Eshombre = true;
 List<SuperPoder>  poderesSpiderman = new List<SuperPoder>();
 poderesSpiderman.Add(poderVolar);
 poderesSpiderman.Add(superFuerza);
-spiderman.SuperPoderes = poderesSpiderman;  
+spiderman.SuperPoderes = poderesSpiderman;
+spiderman.UsarSuperPoderes();
 
+aironman.Id = 2;
+aironman.Name = "Airon Man";
+aironman.IdentidadSecreta = "Tony Stark";
+aironman.Ciudad = "New york";
+aironman.Eshombre = true;
+
+List<SuperPoder> poderesAironman = new List<SuperPoder>();
+poderesAironman.Add(poderVolar);
+poderesAironman.Add(superFuerza);
+aironman.SuperPoderes = poderesAironman;
+aironman.UsarSuperPoderes();
+
+hulk.Id = 3;
+hulk.Name = "Hulk";
+hulk.IdentidadSecreta = "Bruce Banner";
+hulk.Ciudad = "Daytona";
+hulk.Eshombre = true;
+
+List<SuperPoder> poderesHulk = new List<SuperPoder>();
+poderesHulk.Add(poderVolar);
+poderesHulk.Add(superFuerza);
+hulk.SuperPoderes = poderesHulk;
+hulk.UsarSuperPoderes();
 
 class SuperHeroe
 {
@@ -34,6 +70,21 @@ class SuperHeroe
     public List<SuperPoder> SuperPoderes; //array!!!
     public bool PuedeVolar;
     public bool Eshombre;
+
+    public SuperHeroe()
+    {
+        Id = 1;
+        SuperPoderes = new List<SuperPoder>();
+        PuedeVolar = false;
+    }
+
+    public void UsarSuperPoderes()
+    {
+        foreach (var item in SuperPoderes)
+        {
+            Console.WriteLine($"{Name} esta usando el super poder {item.Nombre}!!");
+        }
+    }
 }
 
 class SuperPoder
@@ -41,11 +92,14 @@ class SuperPoder
     public string Nombre;
     public string Descripcion;
     public NivelPoder Nivel;
+
+    public SuperPoder()
+    {
+        Nivel = NivelPoder.NivelUno;
+    }
 }
 
 enum NivelPoder
 {
-    NivelUno,
-    NivelDos,
-    NivelTres,
+    NivelUno,NivelDos,NivelTres,
 }
